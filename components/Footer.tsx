059101ca5dcc8cc612b84d8ac172103a5d140934
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-06-23 00:00:53 +0530

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,20 @@ import { CustomButton } from './CustomButton'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col text-black-500 mt-5 border-t border-gray-500'>
+        <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+            <div className='flex flex-col justify-start items-start gap-6'>
+                <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
+                <p className='text-base text-gray-700'>
+
+                    CarHub <br />
+                    All rights reserved  .
+                </p>
+            </div>
+            
+        </div>
+
+    </footer>
    
   )
 }
