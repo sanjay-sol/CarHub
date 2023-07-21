2f938bce0fa9678b3227020f116700706322428e
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-21 12:58:50 +0530

    searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,17 @@
+"use client"
 import React from 'react'
-
+import {SearchManufacturer} from './';
+import { useState } from 'react';
 const SearchBar = () => {
+    const[manufacturer , setmanufacturer] = useState('');
     const handleSearch = ()=>{}
   return (
    <form className='searchbar' onSubmit={handleSearch} action="">
     <div className='searchbar__item'>
-         
+         <SearchManufacturer 
+         manufacturer={manufacturer}
+         setmanufacturer={setmanufacturer}
+         />
     </div>
    </form>
   )
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,7 @@ export interface CustomButtonProps{
     handleClick?:MouseEventHandler<HTMLButtonElement>;
     btnType?:"button" | "submit";
 }
+export interface SearchManufacturerProps{
+    manufacturer:String;
+    setmanufacturer: (manufacturer:String)=> void;
+}
