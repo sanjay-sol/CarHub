f09446a11f32e6333c4173b25ac936f595760f41
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-21 18:53:48 +0530

    transition card details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -16,11 +16,38 @@ const CarDetails = ({isOpen , closeModal , car}:CarDetailsProps) => {
 <>
 <Transition appear show={isOpen} as={Fragment} >
 <Dialog as="div" className="relative z-10 " onClose={closeModal} >
-    <Transition.Child>
-        <div className='fixed inset-0 bg-black' >
+    <Transition.Child  
+    as={Fragment}
+    enter="ease-out duration-300"
+    enterFrom='opacity-0'
+    enterTo='opacity-100'
+    leave="ease-in duration-200"
+    leaveFrom='opcaity-100'
+    leaveTo='opacity-0'
+    >
+        <div className='fixed inset-0 bg-black bg-opacity-25' >
 
         </div>
     </Transition.Child>
+    <div className='fixed inset-0 overflow-y-auto'>
+        <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <Transition.Child  
+    as={Fragment}
+    enter="ease-out duration-300"
+    enterFrom='opacity-0 scale-95'
+    enterTo='opacity-100 scale-100'
+    leave="ease-in duration-200"
+    leaveFrom='opcaity-100 scale-100'
+    leaveTo='opacity-0 scale-95'
+    >
+
+        <Dialog.Panel>
+            
+        </Dialog.Panel>
+    </Transition.Child>
+        </div>
+
+    </div>
 
 </Dialog>
 </Transition>
