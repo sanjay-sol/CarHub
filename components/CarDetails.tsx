ff9258b100ec1ca27acd5d611ac747c21737f332
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-21 19:01:11 +0530

    crad close button

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { CarProps } from '@/types';
 
 import {Dialog , Transition} from "@headlessui/react";
+import { getHash } from 'next/dist/server/image-optimizer';
 
 interface CarDetailsProps {
     isOpen:Boolean;
@@ -41,8 +42,11 @@ const CarDetails = ({isOpen , closeModal , car}:CarDetailsProps) => {
     leaveTo='opacity-0 scale-95'
     >
 
-        <Dialog.Panel>
-            
+        <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+            <button type='button' className='absolute top-2 ' onClick={closeModal}>
+                <Image src="/close.svg" alt="close " width={20} height={20} className='object-contain' />
+
+            </button>
         </Dialog.Panel>
     </Transition.Child>
         </div>
